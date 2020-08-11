6342585ea4144ee6877acea5afd4bc193b9da0f0
gcrosti <gcrosti@gmail.com>
2020-08-11 14:31:52 +0200

    built page generation script

--- file-generator/generate-file.py
+++ file-generator/generate-file.py
@@ -3,21 +3,45 @@
 from yattag import Doc
 from yattag import indent
 import json
+firebaseKey = 'asdkfjhasdlfaxdoifasfjndls'
+from datetime import date
 
 #%% LOAD DATA
 with open("/Users/giuseppecrosti/virtualenvs/pelican/file-generator/brine-599f1-export-sample.json",'r') as f:
-    out = json.loads(f.read())
+    testData = json.loads(f.read())
 
-print(out)
-#%%
-doc, tag, text = Doc().tagtext()
+#%% RETRIEVE INSTANCE IDs
+instanceIds = []
 
-with tag('head'):
-    with tag('title'):
-        text('hello world from the bot!')
-    doc.stag('meta',name='date',content='2020-07-22')
+for instanceId in testData[firebaseKey]:
+    
+    instanceIds.append(instanceId)
+print(instanceIds)
+
+#%% RETRIEVE MEAL NAMES
+mealNames = []
+instanceId = instanceIds[0]
+
+for data in testData[firebaseKey][instanceId]:
+    if data == None:
+        continue
+    mealName = data['MEAL']['name']
+    mealNames.append(mealName)
+
+print(mealNames)
+
+#%% CREATE PAGE FOR EACH MEAL
+todayDate = date.today()
+for name in mealNames:
+    doc, tag, text = Doc().tagtext()
+    with tag('head'):
+        with tag('title'):
+            text(name)
+        doc.stag('meta',name='date',content='2020-07-22')
+        doc.stag('meta',name='category',content=instanceId)
 
 
-with open("/Users/giuseppecrosti/virtualenvs/pelican/content/helloworld.html",'w') as f:
-    f.write(indent(doc.getvalue()))
+    destination = '/Users/giuseppecrosti/virtualenvs/pelican/content/' + name + '.html'
+    with open(destination,'w') as f:
+        f.write(indent(doc.getvalue()))
 # %%
